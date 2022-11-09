56b59205a7651c3db6fab475a547bc41313965f7
litaesp <117449622+litaesp@users.noreply.github.com>
2022-11-09 02:14:07 +0000

    adding user ressources

--- src/app/views/pages/pages-routing.module.ts
+++ src/app/views/pages/pages-routing.module.ts
@@ -53,7 +53,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes)] ,
   exports: [RouterModule]
 })
 export class PagesRoutingModule {
